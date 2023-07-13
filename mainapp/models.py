ef167f899c8d042f6c3a770ac3730c78b5d8932b
arzei0302 <131849398+arzei0302@users.noreply.github.com>
2023-07-13 10:11:34 +0600

    Update models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название категории')
